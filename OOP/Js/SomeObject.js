10120d99ed9f939148409ec909d6495ffd2b19d7
Angelo <angeloadams76@gmail.com>
2019-02-21 12:04:57 -0700

    Mocha prep commit

--- OOP/Js/SomeObject.js
+++ OOP/Js/SomeObject.js
@@ -55,4 +55,89 @@ class Hero{
     get luck(){
         return this.Luck;
     }
-}
+    set strength(value)
+    {
+        if(typeof value == "number")
+        {
+            this._Strength = value;
+        }
+        else
+        {
+            throw Hero.Invalid_Strength_Value;
+        }
+    }
+    set perception(value)
+    {
+        if(typeof value == "number")
+        {
+            this._Perception = value;
+        }
+        else
+        {
+            throw Hero.Invalid_Perception_Value;
+        }
+    }
+    set endurance(value)
+    {
+        if(typeof value == "number")
+        {
+            this._Endurance= value;
+        }
+        else
+        {
+            throw Hero.Invalid_Endurance_Value;
+        }
+    }
+    set charisma(value)
+    {
+        if(typeof value == "number")
+        {
+            this._Charisma = value;
+        }
+        else
+        {
+            throw Hero.Invalid_Charisma_Value;
+        }
+    }
+    set Intelligence(value)
+    {
+        if(typeof value == "number")
+        {
+            this._Intelligence = value;
+        }
+        else
+        {
+            throw Hero.Invalid_Intel_Value;
+        }
+    }
+    set agility(value)
+    {
+        if(typeof value == "number")
+        {
+            this._Agility = value;
+        }
+        else
+        {
+            throw Hero.Invalid_Agil_Value;
+        }
+    }
+    set luck(value){
+        if(typeof value == "number")
+        {
+            this._Luck = value;
+        }
+        else
+        {
+            throw Hero.Invalid_Luck_Value;
+        }
+    } 
+} 
+    Hero.Invalid_Strength_Value = new BadHeroState("Strength Value must be a number")
+    Hero.Invalid_Perception_Value = new BadHeroState("Perception Value must be a number")
+    Hero.Invalid_Endurance_Value = new BadHeroState("Endurance Value must be a number")
+    Hero.Invalid_Charisma_Value = new BadHeroState("Charisma Value must be a number")
+    Hero.Invalid_Intel_Value = new BadHeroState("Intelligence Value must be a number")
+    Hero.Invalid_Agil_Value = new BadHeroState("Agility Value must be a number")
+    Hero.Invalid_Luck_Value = new BadHeroState("Luck Value must be a number")
+    exports.Hero = Hero;
+    exports.BadHeroState = BadHeroState;
